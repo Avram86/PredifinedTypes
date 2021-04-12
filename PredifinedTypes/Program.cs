833dd80154360f48c4457450b4eb6cf1bda8b57e
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-12 21:25:12 +0300

    Boolean work

--- PredifinedTypes/Program.cs
+++ PredifinedTypes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PredefinedTypes;
+using System;
 
 namespace PredifinedTypes
 {
@@ -6,8 +7,61 @@ namespace PredifinedTypes
     {
         static void Main(string[] args)
         {
+            Person p = new Person();
+            if(p.Name!=null && p.Name.Length > 0)
+            {
+                p.SayHowManyLetters();
+            }
+            else
+            {
+                Console.WriteLine("Person's name is not valid!");
+            }
+
+        }
+        private static void Boolean_ConversionFromString()
+        {
             string text = "true";
-            bool valueAsBool = bool.Parse(text);
+
+            if (bool.TryParse(text, out bool valueAsBool))
+            {
+                Console.WriteLine(valueAsBool);
+            }
+            else
+            {
+                Console.WriteLine("String is NOT convertible to bool!");
+            }
+        }
+
+        private static void Boolean_EvaluateExpression()
+        {
+            int nr = 25;
+            bool isEven = nr % 2 == 0;
+            if (isEven)
+            {
+                Console.WriteLine("Number " + nr + " is even");
+            }
+            else
+            {
+                Console.WriteLine("Number " + nr + " is odd");
+            }
+        }
+
+        private static bool ReturnBool(bool value)
+        {
+            Console.WriteLine("Called ReturnBool("+value+" )");
+            return value;
+
+        }
+
+        private static void Boolean_ConditionalVsBitWiseOperators()
+        {
+            Console.WriteLine("Bitwise AND");
+            bool condiftion1 = ReturnBool(false) & ReturnBool(true);
+            Console.WriteLine(condiftion1);
+            Console.WriteLine("--------------------------------------");
+
+            bool condiftion2 = ReturnBool(false) && ReturnBool(true);
+            Console.WriteLine(condiftion2);
         }
     }
 }
